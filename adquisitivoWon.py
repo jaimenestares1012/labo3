946b12f34269235c2b9c4bee6b5cef21a3a7b556
jaimenestares1012 <jaimenestares1012@gmail.com>
2022-12-12 03:16:15 -0500

    se

---
+++ adquisitivoWon.py
@@ -0,0 +1,112 @@
+from pymongo import MongoClient
+from datetime import datetime, timedelta
+import json
+
+client = MongoClient("mongodb+srv://user_jaime:XhA7pqTDWKfQy6Nh@micluster.pns9q58.mongodb.net")
+db  = client.get_database("tesis-won")
+
+def BuscarMongoXNombre(coleccion ,valor):
+    col = db[coleccion]
+    try:
+        col.create_index([('nombre', 'text')])
+        id = col.find({"$text": {"$search": valor}})
+        return id
+    except NameError:
+        print("ERROR")
+        print(NameError)
+
+
+def BuscarMongo(coleccion ,valor):
+    col = db[coleccion]
+    try:
+        id = col.find_one({"nombre": valor})
+        return id
+    except NameError:
+        print("ERROR")
+        print(NameError)
+    print("buscat")
+
+class poderWon():
+    def __init__(self, nombre, categoria, productos):
+        self.nombre = nombre
+        self.categoria = categoria
+        self.productos = productos
+
+    def teardown_method(self):
+        print("aea")
+
+    def logica(self):
+        fechasDisponibles = []
+        i = 1
+        dias = 20
+        while i <= dias:
+            fechaDateTime = datetime.now() - timedelta(i)
+            fechaExtraccion = fechaDateTime.strftime("%Y-%m-%d %H:%M:%S") 
+            fechaExtraccion2 = fechaExtraccion.split(" ")
+            fechaExtraccionclean = fechaExtraccion2[0]
+            fechasDisponibles.append(fechaExtraccionclean)
+            i = i + 1
+
+        data = []
+        for a in self.productos:
+            busquedaProductos = BuscarMongo(a["typeCategory"], a["prod"])
+            data.append(busquedaProductos)
+        dataxdiaGlobal =[]
+        # for b in data:
+        #     print("este es mi b", b)
+        #     for a in fechasDisponibles:
+        #         dataxdia =[]
+        #         print("<-------------- FECHAAA ------------------->", a)
+        #         try:
+        #             datos = {
+        #                 "producto": b["nombre"],
+        #                 "precio": b[a]
+        #             }
+        #             dataxdia.append(datos)
+        #             datos = {}
+        #             print("dataxdia", dataxdia)
+        #         except:
+        #             datos = {}
+                
+        #         dataParcial = {
+        #             "fecha": a,
+        #             "productosXFecha":dataxdia
+        #         }
+        #         print("data parcial", dataParcial)
+        #         dataxdiaGlobal.append(dataParcial)
+        #         # print("dataxdiaGlobal", dataxdiaGlobal)
+        #         dataParcial = {}
+        #         print("<-------------- FIN  ------------------->")
+        
+
+        for a in fechasDisponibles:
+            print(" FECHAAA ---->", a)
+            dataxdia =[]
+            contador = len(data)
+            it = 1
+            for b in data:
+                try:
+                    price = b[a]
+                    price = price.split("S/")
+                    precioLimpio = price[1].replace(" ", "")
+                    precioLimpio = float(precioLimpio)
+                    datos = {
+                        "producto": b["nombre"],
+                        "precio": precioLimpio
+                    }
+                    dataxdia.append(datos)
+                    datos = {}
+                except:
+                    datos = {}
+                
+                dataParcial = {
+                    "fecha": a,
+                    "productosXFecha":dataxdia
+                }
+                
+                if len(dataParcial["productosXFecha"])> 0 and contador == it:
+                    # print("dataParcial", dataParcial)
+                    dataxdiaGlobal.append(dataParcial)
+                dataParcial = {}
+                it = it + 1         
+        return dataxdiaGlobal 
--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from flask_cors import CORS
 from reporting import reporteTipo1
 from reportingtottus import reporteTipo2
 from reportingMetro import reporteTipo3
+
+from adquisitivoWon import poderWon
 app = Flask(__name__)
 CORS(app)
 
@@ -67,6 +69,54 @@ def inicio(tipo):
         return data
     else:
         return "tipo incorrecto"
+
+@app.route('/get/poder/<int:tipo>', methods=[ 'POST'])
+def poderAdquisitivo(tipo):
+    if tipo == 1:
+        json = request.get_json()
+        producto = json["nombreProducto"]
+        categoria = json["categoria"]
+        productos= json["productosList"]
+        inicio = poderWon( producto, categoria, productos) 
+        datos = inicio.logica()
+        data={
+            "codRes": "00",
+            "detalle": "éxito",
+            "data": datos
+        }
+        return data
+    elif tipo == 2:
+        print("es tipo 2", tipo)
+        json = request.get_json()
+        producto = json["nombreProducto"]
+        categoria = json["categoria"]
+        inicio = reporteTipo2( producto, categoria) 
+        datos = inicio.logica()
+        print("datos", datos)
+        data={
+            "codRes": "00",
+            "detalle": "éxito",
+            "data": datos
+        }
+        return data
+    elif tipo == 3:
+        print("es tipo 3", tipo)
+        json = request.get_json()
+        producto = json["nombreProducto"]
+        categoria = json["categoria"]
+        inicio = reporteTipo3( producto, categoria) 
+        datos = inicio.logica()
+        print("datos", datos)
+        data={
+            "codRes": "00",
+            "detalle": "éxito",
+            "data": datos
+        }
+        return data
+    else:
+        return "tipo incorrecto"
+
+
 
 
 if __name__ == "__main__":
