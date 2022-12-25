34b9893877ae18840c9cfefb7c18816d02105e00
jaimenestares1012 <jaimenestares1012@gmail.com>
2022-12-25 15:43:05 -0500

    ss

--- resumenGraficos.py
+++ resumenGraficos.py
@@ -3,6 +3,7 @@ from datetime import datetime, timedelta
 import matplotlib.pyplot as pl
 import json
 import base64
+from os import remove
 
 client = MongoClient("mongodb+srv://user_jaime:XhA7pqTDWKfQy6Nh@micluster.pns9q58.mongodb.net")
 db  = client.get_database("catalogos")
@@ -53,17 +54,16 @@ class resumenRep():
             mien = a["fecha"].replace("2022-", "")
             arrayGlobal.append(a["montoTotal"])
             arrayFechas.append(mien)
+
         
-        print("arrayFechasDolar", arrayFechasDolar)
-        print("arrayDolar", arrayDolar)
+
         # x =[1000,1004,999]
         # y =["2022-12-23", "2022-12-22", "2022-12-21"]
         
-        # # y = ()
-        # print("logica", x)
+        
         pl.plot(arrayFechas, arrayGlobal,  'b--o')
         pl.savefig("data")
-        plt_1 = pl.figure(figsize=(13,4))
+        pl.figure(figsize=(13,4))
         pl.plot(arrayFechasDolar, arrayDolar ,  'b--o')
         pl.savefig("dataDolar")
 
@@ -71,6 +71,9 @@ class resumenRep():
         try:
             rbase64 = get_as_base64('data.png')
             dbase64 = get_as_base64('dataDolar.png')
+            dbase64 =""
+            remove("data.png")
+            remove("dataDolar.png")
             data = {
                 "producto":rbase64,
                 "dolar":dbase64
